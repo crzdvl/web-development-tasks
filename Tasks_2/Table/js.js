49b1581a33780f95ae38bb46deeb1783284dbebf
crzdvl <vik.coder@gmail.com>
2019-09-17 22:05:26 +0300

    Update js.js

--- Tasks_2/Table/js.js
+++ Tasks_2/Table/js.js
@@ -1,71 +1,97 @@
 const GOODS = [
-    {
-      category: 'furniture',
-      name: 'Chair',
-      amount: 1,
-      price: 20
-    },
-    {
-      category: 'supplies',
-      name: 'Gel Pen',
-      amount: 20,
-      price: 2
-    },
-    {
-      category: 'other',
-      name: 'Trash Bin',
-      amount: 1,
-      price: 5
-    },
-    {
-      category: 'furniture',
-      name: 'Sofa',
-      amount: 1,
-      price: 50
-    },
-    {
-      category: 'supplies',
-      name: 'Notebook',
-      amount: 3,
-      price: 3
-    },
-    {
-      category: 'other',
-      name: 'Calendar 2019',
-      amount: 1,
-      price: 3
-    }
-  ];
+  {
+    category: 'furniture',
+    name: 'Chair',
+    amount: 1,
+    price: 20
+  },
+  {
+    category: 'supplies',
+    name: 'Gel Pen',
+    amount: 20,
+    price: 2
+  },
+  {
+    category: 'other',
+    name: 'Trash Bin',
+    amount: 1,
+    price: 5
+  },
+  {
+    category: 'furniture',
+    name: 'Sofa',
+    amount: 1,
+    price: 50
+  },
+  {
+    category: 'supplies',
+    name: 'Notebook',
+    amount: 3,
+    price: 3
+  },
+  {
+    category: 'other',
+    name: 'Calendar 2019',
+    amount: 1,
+    price: 3
+  }
+];
 
 let total = document.getElementById('total');
 let totalPrice = 0;
 let tableContainer = document.getElementById('table-information');
-let container = '<tbody>';
-
+let i = 0;
 uploadElements(GOODS);
 
 
 window.onload = function () {
-  clickListener();
+  sortCategory();
+  sortName();
 }
 
-
-function clickListener() {
-  const element = document.getElementById('category');
+function sortName() {
+  const element = document.getElementById('name');
   element.addEventListener('click', function(event) {
-    removeElements();
-    sortByCategory();
-    uploadElements(GOODS);
+      sortByName();
+      GOODSreverse();
+      uploadElements(GOODS); 
   });
 }
 
-function removeElements() {
-  console.log(GOODS);
-  for(let i = 1; i < GOODS.length ;i++){
-    console.log(i);
-    document.querySelectorAll('tbody tr')[i].style.display = 'none';
-  }
+function sortCategory() {
+  const element = document.getElementById('category');
+  element.addEventListener('click', function(event) {
+      sortByCategory();
+      GOODSreverse();
+      uploadElements(GOODS); 
+  });
 }
+
+function sortByName() {
+  GOODS.sort(function(a, b){
+    if ( a.name < b.name ){
+      return -1;
+    }
+    if ( a.name > b.name ){
+      return 1;
+    }
+    return 0;
+  });
+}
+
+
+
+
+function GOODSreverse() {
+  if(i === 0){
+    GOODS.reverse();
+    i = 1;
+  } else {
+    i = 0;
+  }
+  return i;
+}
+  
 
 function sortByCategory() {
   GOODS.sort(function(a, b){
@@ -79,8 +105,8 @@ function sortByCategory() {
   });
 }
 
-function uploadElements(elements)
-{  
+function uploadElements(elements){  
+  let container = '<tbody>';
   elements.forEach(function (elem) {
     container += '<tr>';
     container += '<td>' + elem.category + '</td>';
@@ -89,45 +115,43 @@ function uploadElements(elements)
     container += '<td>' + elem.price    + '</td>';
     container += '</tr>';
     totalPrice += elem.price * elem.amount;
-});
+  });
   tableContainer.innerHTML = container;
   total.innerHTML = totalPrice + '¥';
 };
 
-
 filter.onclick = function() {
-  let filterCategory = document.getElementById('filter').value;
-  if (filterCategory != '') {
-    for (let i = 0; i < GOODS.length; i++) {
-        if(GOODS[i].category.trim() === filterCategory.trim()){
-          document.querySelectorAll('tbody tr')[i].style.display = 'table-row';
-        } else {
-          document.querySelectorAll('tbody tr')[i].style.display = 'none';
-        }
-    }
-  } else {
-      for (let j = 0; j <= GOODS.length; j++) {
-        document.querySelectorAll('tbody tr')[j].style.display = 'table-row';
-      }
-  }
-}
-
-
-document.getElementById('search').oninput = function () {
-  let val = this.value.trim();
-
-  if (val != '') {  
-    for (let i = 0; i < GOODS.length; i++) {
-      if (GOODS[i].category.search(val) == -1) {
-        document.querySelectorAll('tbody tr')[i].style.display = 'none';
-      } else {
+let filterCategory = document.getElementById('filter').value;
+if (filterCategory != '') {
+  for (let i = 0; i < GOODS.length; i++) {
+      if(GOODS[i].category.trim() === filterCategory.trim()){
         document.querySelectorAll('tbody tr')[i].style.display = 'table-row';
+      } else {
+        document.querySelectorAll('tbody tr')[i].style.display = 'none';
       }
-    };
   }
-  else {
+} else {
     for (let j = 0; j <= GOODS.length; j++) {
       document.querySelectorAll('tbody tr')[j].style.display = 'table-row';
     }
+}
+}
+
+document.getElementById('search').oninput = function () {
+let val = this.value.trim();
+
+if (val != '') {  
+  for (let i = 0; i < GOODS.length; i++) {
+    if (GOODS[i].category.search(val) == -1) {
+      document.querySelectorAll('tbody tr')[i].style.display = 'none';
+    } else {
+      document.querySelectorAll('tbody tr')[i].style.display = 'table-row';
+    }
+  };
+}
+else {
+  for (let j = 0; j <= GOODS.length; j++) {
+    document.querySelectorAll('tbody tr')[j].style.display = 'table-row';
   }
+}
 }
