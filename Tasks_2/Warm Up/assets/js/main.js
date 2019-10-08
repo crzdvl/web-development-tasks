e9e7617f380ace560a5513e0651ec7a7cfcf5b3f
crzdvl <vik.coder@gmail.com>
2019-10-08 16:56:15 +0300

    Edits after 2 review

--- Tasks_2/Warm Up/assets/js/main.js
+++ Tasks_2/Warm Up/assets/js/main.js
@@ -1,7 +1,7 @@
 /*First*/
 button.onclick = function() {
-	let a = parseInt(document.getElementById("value1").value);
-    	b = parseInt(document.getElementById("value2").value),
+	let a = parseInt(document.getElementById('value1').value),
+    	b = parseInt(document.getElementById('value2').value),
 		sum = 0;
 	let min, max;
 	
@@ -12,32 +12,34 @@ button.onclick = function() {
 	}
 	
 	for(;min <= max;min++){
-		let i = Math.abs(min) % 10; 
-		if(i === 2 || i === 3 || i === 7){
+		let number = Math.abs(min) % 10,
+			re = new RegExp('/3$|7$|2$/');
+
+		if(re.test(number)){
 			sum = sum + min;
 		}
 	}
 
-    document.getElementById("suma").innerHTML = sum;
+    document.getElementById('sum').innerHTML = 'Sum = ' + sum;
 };
 
 /*Second*/
 button_2.onclick = function() {
-	let seconds = parseInt(document.getElementById("seconds").value),
+	let seconds = parseInt(document.getElementById('userSeconds').value),
 		date = new Date(null);
 
 	date.setSeconds(seconds);
 	let timeString = date.toISOString().substr(11, 8);
-	document.getElementById("timeString").innerHTML = timeString;
+	document.getElementById('timeString').innerHTML = 'Time = ' + timeString;
 };
 
 /*Third*/
 button_3.onclick = function() {
-	let timeString = document.getElementById("appt-time").value,
-		a = timeString.split(':'); // split it at the colons
-		secondss = (+a[0]) * 3600 + (+a[1]) * 60 + (+a[2]); 
+	const timeString = document.getElementById('appt-time').value;
+	let a = timeString.split(':'); // split it at the colons
+	secondss = (+a[0]) * 3600 + (+a[1]) * 60 + (+a[2]); // 3600s in hour, 60s in minute
 
-	document.getElementById("seconds").innerHTML = secondss;
+	document.getElementById('seconds').innerHTML = 'Seconds = ' + secondss;
 }
 
 
@@ -56,33 +58,25 @@ button_4.onclick = function() {
 		currentDate['day'] = date2.getDate() - date1.getDate();
 		currentDate['month'] = date2.getMonth() - date1.getMonth();
 		currentDate['year'] = date2.getFullYear() - date1.getFullYear();
+		currentDate['hour']   = date2.getHours()    - date1.getHours();
+		currentDate['minute'] = date2.getMinutes()  - date1.getMinutes();
 
-		if (date1.getHours() > date2.getHours()){
-			currentDate['hour']   = date1.getHours()    - date2.getHours();
-		}else {
-			currentData['hour']   = date2.getHours()    - date1.getHours();
-		}
 
-		if (date1.getMinutes() > date2.getMinutes()){
-			currentDate['minute'] = date1.getMinutes()  - date2.getMinutes(); 
-		}else {
-			currentDate['minute'] = date2.getMinutes()  - date1.getMinutes();
-		}
 
-		document.getElementById("datesDifference").innerHTML = ' Прошло дней ' + currentDate['day'] + ' Прошло месяцев: ' + currentDate['month'] +
+		document.getElementById('datesDifference').innerHTML = 'Time = ' + ' Прошло дней ' + currentDate['day'] + ' Прошло месяцев: ' + currentDate['month'] +
 			' Прошло лет: ' + currentDate['year'] + ' Прошло часов: ' + currentDate['hour'] +
 			' Прошло минут: ' + currentDate['minute'];
 	}
 
 
 button_5.onclick = function() { 
-	let a = document.getElementById("num1").value;
-	let b = document.getElementById("num2").value;
+	const a = document.getElementById('num1').value,
+		b = document.getElementById('num2').value;
 	let tableContainer = document.getElementById('tableResult');
 
 	let container = "<table border='1' width='300' height='300'>";
 	for(let i=1; i<= a; i++) {
-		container += "</tr>";
+		container += '</tr>';
 		for(let j=1; j<= b; j++) {
 			if ((i+j)%2!=0){
 				container += "<td bgcolor='white'></td>";
@@ -90,15 +84,15 @@ button_5.onclick = function() {
 			else {
 				container += "<td bgcolor='black'></td>";
 			}}
-			container += "</tr>";
+			container += '</tr>';
 	}
-	container += "</table>";
+	container += '</table>';
 	tableContainer.innerHTML = container;
 }
 
-
-button_6.onclick = function() {
-	let informationn = document.getElementById("area").value;
+const userData = document.getElementById('area');
+userData.addEventListener('blur', function() {
+	let informationn = document.getElementById('area').value,
 		information = informationn.split(','),
 		expression,
 		urlRegexpFirst,
@@ -106,41 +100,40 @@ button_6.onclick = function() {
 		ipRegexp,
 		x = undefined,
 		a = document.createElement('a');
-   
+
 	expression = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
 	urlRegexpFirst = new RegExp(expression);
-   
+
 	expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/gi;
 	urlRegexpSecond = new RegExp(expression);
-   
+
 	expression = /(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}/g;
 	ipRegexp = new RegExp(expression);
-   
+
 	for(let i = 0; i < information.length; i++){
-	 if(information[i].match(urlRegexpFirst) || information[i].match(urlRegexpSecond) || information[i].match(ipRegexp) ) {
-	 } else {
-	  	delete information[i]; //.splice(i, i); //начиная с позиции 1, удалить 1 элемент
-	 }
+		if(information[i].match(urlRegexpFirst) || information[i].match(urlRegexpSecond) || information[i].match(ipRegexp) ) {
+		} else {
+			delete information[i]; //.splice(i, i); //начиная с позиции 1, удалить 1 элемент
+		}
 	}
 
 	information.sort();
 
 	for (let i = 0; i < information.length; i++) {
-	 if(information[i] != x){
-	  	link = a.cloneNode();
-		link.href = information[i];
-		link.innerHTML = information[i] + '</br>';
-		document.getElementById('list').appendChild(link);
-	 }
+		if(information[i] != x){
+			link = a.cloneNode();
+			link.href = information[i];
+			link.innerHTML = information[i] + '</br>';
+			document.getElementById('list').appendChild(link);
+		}
 	}
-}
+});
 
 button_7.onclick = function() {
 	let text = document.getElementById('text').value,
-		regexp = new RegExp(document.getElementById('regexp').value);
+		regexp = new RegExp(document.getElementById('regexp').value, 'gm');
 
 	text = text.replace(regexp, '<mark>$&</mark>');
 
-	document.getElementById("newText").innerHTML = text;
-}
-
+	document.getElementById('newText').innerHTML = 'Marked text: ' + text;
+};
