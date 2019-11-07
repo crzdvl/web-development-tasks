96f908f731118ae9e38595c54e2dd56cb828ae95
crzdvl <vik.coder@gmail.com>
2019-11-07 22:52:32 +0200

    Create arrowUp.js

---
+++ Tasks_3/Landing/assets/js/arrowUp.js
@@ -0,0 +1,22 @@
+//scroll
+$(document).ready(function(){
+    $("#menu").on("click","a", function (event) {
+        event.preventDefault();
+        let id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+});
+
+//arrow
+$(function() {
+    $(window).scroll(function() { 
+        if($(this).scrollTop() != 0) {   
+            $('#toTop').fadeIn();
+        } else {
+            $('#toTop').fadeOut();  
+        }});  
+        $('#toTop').click(function() {  
+        $('body,html').animate({scrollTop:0},800); 
+    }); 
+});
+});
