337e9e6dc34dc1d57d7c3c48b573d5b14465ce9b
crzdvl <vik.coder@gmail.com>
2019-11-13 12:27:15 +0200

    Update main.js

--- Tasks_2/Warm Up/assets/js/main.js
+++ Tasks_2/Warm Up/assets/js/main.js
@@ -2,8 +2,9 @@
 button.onclick = function() {
 	let a = parseInt(document.getElementById('value1').value),
     	b = parseInt(document.getElementById('value2').value),
-		sum = 0;
-	let min, max;
+		sum = 0,
+		min, max,
+		re = /3$|7$|2$/;
 	
 	if(a < b){
 		[max, min] = [b, a];
@@ -12,11 +13,8 @@ button.onclick = function() {
 	}
 	
 	for(;min <= max;min++){
-		let number = Math.abs(min) % 10,
-			re = new RegExp('/3$|7$|2$/');
-
-		if(re.test(number)){
-			sum = sum + min;
+		if(re.test(min)){
+			sum += min;
 		}
 	}
 
@@ -27,10 +25,13 @@ button.onclick = function() {
 button_2.onclick = function() {
 	let seconds = parseInt(document.getElementById('userSeconds').value),
 		date = new Date(null);
-
-	date.setSeconds(seconds);
+	if (seconds > 99999999999){
+		document.getElementById('timeString').innerHTML =  'Введите меньшее число';
+	} else {
+		date.setSeconds(seconds);
 	let timeString = date.toISOString().substr(11, 8);
 	document.getElementById('timeString').innerHTML = 'Time = ' + timeString;
+	}
 };
 
 /*Third*/
@@ -38,8 +39,11 @@ button_3.onclick = function() {
 	const timeString = document.getElementById('appt-time').value;
 	let a = timeString.split(':'); // split it at the colons
 	secondss = (+a[0]) * 3600 + (+a[1]) * 60 + (+a[2]); // 3600s in hour, 60s in minute
-
-	document.getElementById('seconds').innerHTML = 'Seconds = ' + secondss;
+	if (Object.is(secondss, NaN)){
+		document.getElementById('seconds').innerHTML = 'Введите значение';
+	} else {
+		document.getElementById('seconds').innerHTML = 'Seconds = ' + secondss;
+	}
 }
 
 
@@ -50,22 +54,44 @@ button_4.onclick = function() {
 
 		date1 = new Date(date1.value);
 		date2 = new Date(date2.value);
-		
-		if (date1 > date2) {
-			[date1 , date2] = [date2, date1];
-		}
 
-		currentDate['day'] = date2.getDate() - date1.getDate();
-		currentDate['month'] = date2.getMonth() - date1.getMonth();
-		currentDate['year'] = date2.getFullYear() - date1.getFullYear();
-		currentDate['hour']   = date2.getHours()    - date1.getHours();
-		currentDate['minute'] = date2.getMinutes()  - date1.getMinutes();
+		if (!date1.getTime() || !date2.getTime()) {
+			document.getElementById('datesDifference').innerHTML = ' Введите значения';
+		} else {
+			if (date1.getDate() < date2.getDate()){
+				currentDate['day'] = date2.getDate() - date1.getDate();
+			} else {
+				currentDate['day'] = date1.getDate() - date2.getDate();
+			}
+	
+			if (date1.getMonth() < date2.getMonth()){
+				currentDate['month'] = date2.getMonth() - date1.getMonth();
+			} else {
+				currentDate['month'] = date1.getMonth() - date2.getMonth();
+			}
+	
+			if (date1.getFullYear() < date2.getFullYear()){
+				currentDate['year'] = date2.getFullYear() - date1.getFullYear();
+			} else {
+				currentDate['year'] = date1.getFullYear() - date2.getFullYear();
+			}
+	
+			if (date1.getHours() < date2.getHours()){
+				currentDate['hour']  = date2.getHours()    - date1.getHours();
+			} else {
+				currentDate['hour']  = date1.getHours()    - date2.getHours();
+			}
+	
+			if (date1.getMinutes() < date2.getMinutes()){
+				currentDate['minute'] = date2.getMinutes()  - date1.getMinutes();
+			} else {
+				currentDate['minute'] = date1.getMinutes()  - date2.getMinutes();
+			}
 
-
-
-		document.getElementById('datesDifference').innerHTML = 'Time = ' + ' Прошло дней ' + currentDate['day'] + ' Прошло месяцев: ' + currentDate['month'] +
+			document.getElementById('datesDifference').innerHTML = ' Прошло дней ' + currentDate['day'] + ' Прошло месяцев: ' + currentDate['month'] +
 			' Прошло лет: ' + currentDate['year'] + ' Прошло часов: ' + currentDate['hour'] +
 			' Прошло минут: ' + currentDate['minute'];
+		}
 	}
 
 
@@ -93,13 +119,14 @@ button_5.onclick = function() {
 const userData = document.getElementById('area');
 userData.addEventListener('blur', function() {
 	let informationn = document.getElementById('area').value,
-		information = informationn.split(','),
 		expression,
 		urlRegexpFirst,
 		urlRegexpSecond,
 		ipRegexp,
 		x = undefined,
 		a = document.createElement('a');
+
+	let information = informationn.split(',');
 
 	expression = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
 	urlRegexpFirst = new RegExp(expression);
@@ -112,6 +139,7 @@ userData.addEventListener('blur', function() {
 
 	for(let i = 0; i < information.length; i++){
 		if(information[i].match(urlRegexpFirst) || information[i].match(urlRegexpSecond) || information[i].match(ipRegexp) ) {
+			console.log(information[i]);
 		} else {
 			delete information[i]; //.splice(i, i); //начиная с позиции 1, удалить 1 элемент
 		}
@@ -123,7 +151,7 @@ userData.addEventListener('blur', function() {
 		if(information[i] != x){
 			link = a.cloneNode();
 			link.href = information[i];
-			link.innerHTML = information[i] + '</br>';
+			link.innerHTML = information[i].replace(/(http:\/\/|www\.|https:\/\/www\.|http:\/\/|https:\/\/)/gm, '') + '</br>';
 			document.getElementById('list').appendChild(link);
 		}
 	}
