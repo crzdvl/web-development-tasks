d6932e2fef92c8e3c32d92c0fcd5a76af990373e
crzdvl <vik.coder@gmail.com>
2019-12-15 16:39:21 +0200

    Revert "Made 4 task"
    This reverts commit b7fbfa0ff0e73a1c870e81b33aba204b5db52064.

--- Tasks_4/Warm up/php/eighth-task.php
+++
@@ -1,9 +0,0 @@
-<?php
-    session_start();
-
-    $userText = $_POST['value-eighth-task'];
-
-    $_SESSION['result-eighth-task'] = mb_strlen($userText, 'utf-8');
-
-    header('Location: /index.php');
-?>
