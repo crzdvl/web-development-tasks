99e9d081809db1fb4a5a504a55e734cbc31ae0ee
crzdvl <vik.coder@gmail.com>
2019-12-24 17:02:56 +0200

    Fixed after mentor's review

--- webdev-st1.5-slider-master/slider-template.js
+++ webdev-st1.5-slider-master/slider-template.js
@@ -11,56 +11,63 @@ const IMAGES = [
   '?image=1039'
 ];
 
-//upload photos
-for (let i in  IMAGES){
-  $('.slider-previews').append(
-    `<li>
-        <img src="${API_URL}${SMALL_SIZE}${IMAGES[i]}"
-             idImage="${IMAGES[i]}" imageValue="${i}">
-      </li>`
-  );
-};
+$(function() { 
   
-let currentImageAttr,
-    currentImageValue = $('li img').attr('imageValue');
+  //upload photos
+  for (let i in  IMAGES){
+    $('.slider-previews').append(
+      `<li>
+          <img src="${API_URL}${SMALL_SIZE}${IMAGES[i]}"
+              idImage="${IMAGES[i]}" imageValue="${i}">
+        </li>`
+    );
+  };
+    
+  let currentImageAttr,
+      currentImageValue = $('#slider li img').attr('imageValue');
 
-//switching photos on click
-$("li img").click(function () {
-  currentImageAttr = $(this).attr('idImage');
-  changeTheImage(currentImageAttr);
-});
-
-//switching photos with keys
-$(window).keyup(event, () => {
-  const leftButton = 37,
-        rightButton = 39;
-
-  if (event.which == leftButton) {
-    if (currentImageValue <= 0) {
-      currentImageValue = 6;
-    }
-      currentImageValue--;
-      currentImageAttr = IMAGES[currentImageValue];
-  } else if (event.which == rightButton) {
-    if (currentImageValue >= 5) {
-      currentImageValue = -1;
-    }
-    currentImageValue++;
-    currentImageAttr = IMAGES[currentImageValue];
-    }
+  //switching photos on click
+  $("#slider li img").click(function () {
+    currentImageAttr = $(this).attr('idImage');
     changeTheImage(currentImageAttr);
+  });
+
+  //switching photos with keys
+  $(window).keyup(event, () => {
+    const leftButton = 37,
+          rightButton = 39;
+
+    if (event.which == leftButton) {
+      if (currentImageValue <= 0) {
+        currentImageValue = IMAGES.length - 1; //start from 5-th index image
+        currentImageAttr = IMAGES[currentImageValue];
+        console.log(currentImageValue);
+      } else {
+        currentImageValue--;
+        currentImageAttr = IMAGES[currentImageValue];
+      }
+    } else if (event.which == rightButton) {
+      if (currentImageValue >= 5) {
+        currentImageValue = IMAGES.length - IMAGES.length; // start from 0 index image
+        currentImageAttr = IMAGES[currentImageValue];
+      } else {
+        currentImageValue++;
+        currentImageAttr = IMAGES[currentImageValue];
+      }
+      }
+      changeTheImage(currentImageAttr);
+
+  });
+
+  function changeTheImage(currentImageAttr) {
+    //remove classes which added before
+    $('.current').removeClass('current');
+
+    //add class for the current preview image selected by user
+    $(`img[idImage="${currentImageAttr}"]`).parent().addClass('current'); 
+
+    //change the image to the selected image by the user
+    $('.slider-current img').attr("src",`${API_URL}${BIG_SIZE}/${currentImageAttr}`);
+  };
 
 });
-
-function changeTheImage(currentImageAttr) {
-  //remove classes which added before
-  $('.current').removeClass('current');
-
-  //add class for the current preview image selected by user
-  $(`img[idImage="${currentImageAttr}"]`).parent().addClass('current'); 
-
-  //change the image to the selected image by the user
-  $('.slider-current img').attr("src",`${API_URL}${BIG_SIZE}/${currentImageAttr}`);
-};
-
-
