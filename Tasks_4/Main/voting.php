a65d096cb0faaf5d7ea4a9a225d10372c0e06ac6
crzdvl <vik.coder@gmail.com>
2019-12-27 12:19:35 +0200

    Fixed after first review

--- Tasks_4/Main/voting.php
+++ Tasks_4/Main/voting.php
@@ -1,3 +1,23 @@
+<?php
+$userVote = $_POST['radio-button'];
+if (isset($userVote)){
+  $userVote = $_POST['radio-button'];
+  $json_file = file_get_contents('vote-result.json');
+  $json_file = json_decode($json_file, true);
+  $google_chart = [array('answer', 'votes')];
+
+  $json_file[$userVote] += 1; 
+
+  foreach ($json_file as $key => $value) {
+    $i = array($key, $value);
+    $google_chart[] = $i;
+  }
+  file_put_contents('vote-result.json',json_encode($json_file));
+} else {
+  header("Location: index.php");
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,33 +26,26 @@
     <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
     <script type="text/javascript">
       window.onload = function() {
+        
         google.charts.load("current", {packages:["corechart"]});
         google.charts.setOnLoadCallback(drawChart);
-
         function drawChart() {
-          const jsonData = $.ajax({
-            url: "vote-result.json",
-            dataType: "json",
-            async: false
-            }).responseText;   
-          
+          var data = google.visualization.arrayToDataTable(<?php echo json_encode($google_chart)?>);
+
           let options = {
-            width: 550,
-            height: 600,
-            colors: ['#6bbb1c', '#1cbba1', '#1c93bb']
+                width: 550,
+                height: 600,
+                colors: ['#6bbb1c', '#1cbba1', '#1c93bb']
           };
 
-          let data = new google.visualization.DataTable(jsonData);
-
-          let chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-          chart.draw(data,  options);
+          var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+          chart.draw(data, options);
         }
       }
     </script>
   </head>
   <body>
       <h1>What is the worst - to fail or not try?</h1>
-
       <div id="chart_div" style="
         justify-content: center;
         align-items: center;
@@ -40,23 +53,3 @@
       </div>
   </body>
 </html>
-
-<?php
-  /*ini_set('error_reporting', E_ALL);
-  ini_set('display_errors', 1);
-  ini_set('display_startup_errors', 1);*/
-
-  $userVote = $_POST['radio-button'];
-
-  $data = file_get_contents('vote-result.json');
-            
-  $list = json_decode($data,TRUE);
-  
-  for ($i = 0; $i <= 2; $i++) {
-    if ($userVote == $list['rows'][$i]['c'][0]['v']) {
-      $list['rows'][$i]['c'][1]['v'] += 1;
-    }
-    }
-
-  file_put_contents('vote-result.json',json_encode($list));
-?>
